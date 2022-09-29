ab276d6b66b420fdd26efd0d02bc153d8899162f
LuisR19 <A01639766@itesm.mx>
2022-09-29 13:31:21 +0000

    Practica 1

---
+++ fork.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+int main(){
+    int pid = fork();
+
+    if(pid == 0){
+        printf("Soy el proceso hijo\n");
+        execl("/workspace/TC1004B.514-/hola.exe","hola.exe",NULL);
+        sleep(60);
+        printf("nunca nunca nunca ");
+    }else
+        {
+            printf("Soy el proceso padre\n");
+            wait(NULL);
+        }
+    return 0;
+}
